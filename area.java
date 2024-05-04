6bf5f83262e225225c05a2b2cd27fc9471d64678
Hao <hxuan7688@gmail.com>
2024-05-05 02:05:02 +0800

    idk

--- area.java
+++ area.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class area {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
-            int choice;
+            int choice1;
             while (true) {
             System.out.println("1.Enter diameter");
             System.out.println("2.Enter radius");
             if (input.hasNextInt()) {
-                choice = input.nextInt();
-                if (choice == 1 || choice == 2) {
+                choice1 = input.nextInt();
+                if (choice1== 1 || choice1 == 2) {
                     break;
                 } else {
                     System.err.println("Invalid choice. Please enter 1 or 2.");
@@ -19,24 +19,25 @@ public class area {
             }
             }
 
-            switch (choice) {
+            switch (choice1) {
                 case 1:
                     System.out.println("Enter diameter: ");
                     double diameter = input.nextDouble();
                     double radius_1 = diameter / 2;
-                    double area_1 = Math.PI * (radius_1 * radius_1);
-                    System.out.println("Area of circle: " + area_1);
+                    //double area_1 = Math.PI * (radius_1 * radius_1);
+                    //System.out.println("Area of circle: " + area_1);
                     break;
                 case 2:
                     System.out.println("Enter radius: ");
                     double radius_2 = input.nextDouble();
-                    double area_2 = Math.PI * (radius_2 * radius_2);
-                    System.out.println("Area of circle: " + area_2);
+                    //double area_2 = Math.PI * (radius_2 * radius_2);
+                    //System.out.println("Area of circle: " + area_2);
                     break;
 
                 default:
                     System.out.println("Invalid choice. Please enter 1 or 2.");
                     break;
+            
             }
         }
     }
